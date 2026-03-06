6e07f9762085e832a4cb923cef96e35647dcf32c
dominicuyamotpino-cmd <dominicuyamotpino@gmail.com>
2026-03-06 18:54:37 +0800

    Update script.js

--- script.js
+++ script.js
@@ -5,4 +5,33 @@ function showLab(labId){
     document.getElementById("lab3").style.display = "none";
 
     document.getElementById(labId).style.display = "flex";
+
 }
+
+function showLab(labId) {
+
+let labs = document.querySelectorAll(".lab-gallery");
+
+labs.forEach(function(lab){
+lab.style.display = "none";
+});
+
+document.getElementById(labId).style.display = "flex";
+}
+
+const form = document.forms['submit-to-google-sheet']
+const msg = document.getElementById("msg")
+
+form.addEventListener('submit', e => {
+e.preventDefault()
+
+fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+.then(response => {
+msg.innerHTML = "Message sent successfully!"
+setTimeout(function(){
+msg.innerHTML = ""
+},5000)
+form.reset()
+})
+.catch(error => console.error('Error!', error.message))
+})
